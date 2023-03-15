2b196d18fbeb2f3c6b464ceb921fd5cbcb76adc2
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-15 17:02:02 +0700

    🎉 init nvim
    copy from
    
    https://github.com/josean-dev/dev-environment-files
    
    https://www.youtube.com/watch?v=vdn_pKJUda8

---
+++ .config/nvim/lua/micky/core/colorscheme.lua
@@ -0,0 +1,6 @@
+local status, _ = pcall(vim.cmd, "colorscheme onedark")
+if not status then
+    print("Colorscheme not found!")
+    return
+end
+
---
+++ .config/nvim/lua/micky/plugins/nvim-tree.lua
@@ -0,0 +1,34 @@
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+    return
+end
+
+-- recommended settings from nvim-tree documentation
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+nvimtree.setup({
+  -- change folder arrow icons
+  renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          arrow_closed = "", -- arrow when folder is closed
+          arrow_open = "", -- arrow when folder is open
+        },
+      },
+    },
+  },
+    actions = {
+        open_file = {
+            window_picker = {
+                enable = false,
+            },
+        }
+    }
+
+
+
+})
