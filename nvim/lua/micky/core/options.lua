3f01f9c899129dc4dc2b783a088dd47881a683ad
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-19 14:07:04 +0700

    :sparkles: disabled swap file and speeds up updatetime

--- nvim/lua/micky/core/options.lua
+++ nvim/lua/micky/core/options.lua
@@ -35,5 +35,9 @@ opt.clipboard:append("unnamedplus")
 opt.splitright = true
 opt.splitbelow = true
 
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
-
+opt.updatetime = 50
