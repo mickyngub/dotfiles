4d626e82deb2452e4215680dce9785a106b49cc1
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-19 14:10:28 +0700

    :sparkles: adds keymaps

--- nvim/lua/micky/core/keymaps.lua
+++ nvim/lua/micky/core/keymaps.lua
@@ -4,6 +4,18 @@ local keymap = vim.keymap -- for conciseness
 
 -- general keymaps
 
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+keymap.set("n", "J", "mzJ`z")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+keymap.set("x", "<leader>p", [["_dP]])
+keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>")
 -- clear search highlights
@@ -26,7 +38,7 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
--- plugin 
+-- plugin
 
 -- vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
@@ -36,6 +48,7 @@ keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>")
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
