db5a19a5ce241f6965018fa300e9bf449d788626
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-19 14:02:51 +0700

    :lipstick: make neovim bg transparent

--- nvim/lua/micky/core/colorscheme.lua
+++ nvim/lua/micky/core/colorscheme.lua
@@ -1,6 +1,7 @@
 local status, _ = pcall(vim.cmd, "colorscheme onedark")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 if not status then
-    print("Colorscheme not found!")
-    return
+	print("Colorscheme not found!")
+	return
 end
-
