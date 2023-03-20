68e8beb65161029bc6e18d4743055437b961c4d7
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-20 22:44:57 +0700

    :sparkles: adds keymap for toggling git blame

--- nvim/lua/micky/plugins/gitsigns.lua
+++ nvim/lua/micky/plugins/gitsigns.lua
@@ -4,5 +4,16 @@ if not setup then
 	return
 end
 
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
+
 -- configure/enable gitsigns
-gitsigns.setup()
+gitsigns.setup({
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 100,
+		ignore_whitespace = false,
+	},
+})
