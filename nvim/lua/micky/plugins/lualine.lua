b387dad3ac90b86bbe9e7649a48aa786ff87f260
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-20 23:34:35 +0700

    :sparkles: shows relative path instead of absolute path

--- nvim/lua/micky/plugins/lualine.lua
+++ nvim/lua/micky/plugins/lualine.lua
@@ -34,7 +34,7 @@ lualine.setup({
 			{
 				"filename",
 				file_status = true, -- displays file status (readonly status, modified status)
-				path = 2, -- 0 = just filename, 1 = relative path, 2 = absolute path
+				path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
 			},
 		},
 	},
