8524b52c2430ada8fa5d4586cee033b4e78a0c62
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-25 14:52:14 +0700

    Revert ":heavy_plus_sign: uses zbirenbaum/copilot instead of github"
    This reverts commit 2a8b5a18131b7c56568fd4774d6430b2e07d8e0c.

--- nvim/lua/micky/plugins-setup.lua
+++ nvim/lua/micky/plugins-setup.lua
@@ -163,56 +163,9 @@ return packer.startup(function(use)
 		requires = { "nvim-tree/nvim-web-devicons" },
 	})
 	use("dstein64/vim-startuptime")
-	use({
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				panel = {
-					enabled = true,
-					auto_refresh = false,
-					keymap = {
-						jump_prev = "[[",
-						jump_next = "]]",
-						accept = "<CR>",
-						refresh = "gr",
-						open = "<M-CR>",
-					},
-					layout = {
-						position = "bottom", -- | top | left | right
-						ratio = 0.4,
-					},
-				},
-				suggestion = {
-					enabled = true,
-					auto_trigger = true,
-					debounce = 75,
-					keymap = {
-						accept = "<C-i>",
-						accept_word = false,
-						accept_line = false,
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<C-]>",
-					},
-				},
-				filetypes = {
-					yaml = false,
-					markdown = false,
-					help = false,
-					gitcommit = false,
-					gitrebase = false,
-					hgcommit = false,
-					svn = false,
-					cvs = false,
-					["."] = false,
-				},
-				copilot_node_command = "node", -- Node.js version must be > 16.x
-				server_opts_overrides = {},
-			})
-		end,
-	})
+
+	use("github/copilot.vim")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
