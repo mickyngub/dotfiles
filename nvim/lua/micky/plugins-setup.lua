e746ea7631fa5ea3cdf59200dd8147c7de11ab52
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-25 19:21:45 +0700

    🎨 runs prettier

--- nvim/lua/micky/plugins-setup.lua
+++ nvim/lua/micky/plugins-setup.lua
@@ -11,66 +11,65 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
-require("lazy").setup(
-{
+require("lazy").setup({
 	-- lua functions that many plugins use
-		"nvim-lua/plenary.nvim",
-		"joshdick/onedark.vim",
-		"christoomey/vim-tmux-navigator", -- tmux & split window navigation
-		"szw/vim-maximizer", -- maximizes and restores current window
-		-- essential plugins
-		"tpope/vim-surround",
-		"vim-scripts/ReplaceWithRegister",
-		-- commenting with gc
-		"numToStr/Comment.nvim",
-		-- file explorer
-		"nvim-tree/nvim-tree.lua",
-		-- icons
-		"nvim-tree/nvim-web-devicons",
-		-- statusline
-		"nvim-lualine/lualine.nvim",
-		-- fuzzy finding w/ telescope
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- dependency for better sorting performance
-		{ "nvim-telescope/telescope.nvim", branch = "0.1.x" }, -- fuzzy finder
-		-- autocompletion
-		"hrsh7th/nvim-cmp", -- completion plugin
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		-- snippets
-		"L3MON4D3/LuaSnip", -- snippet engine
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
-		-- managing & installing lsp servers, linters & formatters
-		"williamboman/mason.nvim", -- in charge of managing lsp servers, linters & formatters
-		"williamboman/mason-lspconfig.nvim", -- bridges gap b/w mason & lspconfig
-		-- configuring lsp servers
-		"neovim/nvim-lspconfig", -- easily configure language servers
-		"hrsh7th/cmp-nvim-lsp", -- for autocompletion
-		"hrsh7th/cmp-nvim-lsp-signature-help", -- for hinting function signature
-		{
-			"glepnir/lspsaga.nvim",
-			branch = "main",
-			dependencies = {
-				{ "nvim-tree/nvim-web-devicons" },
-				{ "nvim-treesitter/nvim-treesitter" },
-			},
+	"nvim-lua/plenary.nvim",
+	"joshdick/onedark.vim",
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"szw/vim-maximizer", -- maximizes and restores current window
+	-- essential plugins
+	"tpope/vim-surround",
+	"vim-scripts/ReplaceWithRegister",
+	-- commenting with gc
+	"numToStr/Comment.nvim",
+	-- file explorer
+	"nvim-tree/nvim-tree.lua",
+	-- icons
+	"nvim-tree/nvim-web-devicons",
+	-- statusline
+	"nvim-lualine/lualine.nvim",
+	-- fuzzy finding w/ telescope
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- dependency for better sorting performance
+	{ "nvim-telescope/telescope.nvim", branch = "0.1.x" }, -- fuzzy finder
+	-- autocompletion
+	"hrsh7th/nvim-cmp", -- completion plugin
+	"hrsh7th/cmp-buffer", -- source for text in buffer
+	"hrsh7th/cmp-path", -- source for file system paths
+	-- snippets
+	"L3MON4D3/LuaSnip", -- snippet engine
+	"saadparwaiz1/cmp_luasnip", -- for autocompletion
+	"rafamadriz/friendly-snippets", -- useful snippets
+	-- managing & installing lsp servers, linters & formatters
+	"williamboman/mason.nvim", -- in charge of managing lsp servers, linters & formatters
+	"williamboman/mason-lspconfig.nvim", -- bridges gap b/w mason & lspconfig
+	-- configuring lsp servers
+	"neovim/nvim-lspconfig", -- easily configure language servers
+	"hrsh7th/cmp-nvim-lsp", -- for autocompletion
+	"hrsh7th/cmp-nvim-lsp-signature-help", -- for hinting function signature
+	{
+		"glepnir/lspsaga.nvim",
+		branch = "main",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+			{ "nvim-treesitter/nvim-treesitter" },
 		},
-		-- enhanced lsp uis
-		"jose-elias-alvarez/typescript.nvim", -- additional functionality for typescript server (e.g. rename file & update imports)
-		"onsails/lspkind.nvim", -- vs-code like icons for autocompletion
-		-- formatting & linting
-		"jose-elias-alvarez/null-ls.nvim", -- configure formatters & linters
-		"jayp0521/mason-null-ls.nvim", -- bridges gap b/w mason & null-ls
-		-- treesitter configuration
-		{
-			"nvim-treesitter/nvim-treesitter",
-			build = function()
-				local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-				ts_update()
-			end,
-			dependencies = { "windwp/nvim-ts-autotag" },
-		},
-	
+	},
+	-- enhanced lsp uis
+	"jose-elias-alvarez/typescript.nvim", -- additional functionality for typescript server (e.g. rename file & update imports)
+	"onsails/lspkind.nvim", -- vs-code like icons for autocompletion
+	-- formatting & linting
+	"jose-elias-alvarez/null-ls.nvim", -- configure formatters & linters
+	"jayp0521/mason-null-ls.nvim", -- bridges gap b/w mason & null-ls
+	-- treesitter configuration
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+		dependencies = { "windwp/nvim-ts-autotag" },
+	},
+
 	"nvim-treesitter/playground",
 	-- git integration
 	"lewis6991/gitsigns.nvim", -- show line modifications on left hand side
@@ -141,6 +140,5 @@ require("lazy").setup(
 				},
 			})
 		end,
-	}
+	},
 })
-
