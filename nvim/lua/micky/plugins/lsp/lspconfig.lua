ab4c25894824e7c02fc78bff5c67e523772434bf
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-28 01:40:35 +0700

    :sparkles: adds peek type definition keymap

--- nvim/lua/micky/plugins/lsp/lspconfig.lua
+++ nvim/lua/micky/plugins/lsp/lspconfig.lua
@@ -28,6 +28,7 @@ local on_attach = function(client, bufnr)
 	keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
 	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
 	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+	keymap.set("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
 	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
 	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
 	keymap.set("n", "<leader>dl", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
