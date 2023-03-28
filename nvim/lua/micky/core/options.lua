3ad845b4e59cd5520241757d8ef626f3e07d9987
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-03-29 00:45:01 +0700

    :wrench: decreases tabstop and shiftwidth from 4 to 2

--- nvim/lua/micky/core/options.lua
+++ nvim/lua/micky/core/options.lua
@@ -7,8 +7,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
