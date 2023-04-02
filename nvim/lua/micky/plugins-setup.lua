4ce003c000afa9ec8c19c03290cf4ec48663c126
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-04-03 01:15:37 +0700

    ➕ adds diffview

--- nvim/lua/micky/plugins-setup.lua
+++ nvim/lua/micky/plugins-setup.lua
@@ -13,7 +13,12 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 require("lazy").setup({
 	-- lua functions that many plugins use
-	"nvim-lua/plenary.nvim",
+	{
+		"nvim-lua/plenary.nvim",
+		dependencies = {
+			{ "sindrets/diffview.nvim" },
+		},
+	},
 	"joshdick/onedark.vim",
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
 	"szw/vim-maximizer", -- maximizes and restores current window
