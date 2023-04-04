99291b83033635d5c2e728442f417687645d03f9
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-04-04 16:19:27 +0700

    :sparkles: adds fugitive keymaps

--- nvim/lua/micky/plugins/fugitive.lua
+++ nvim/lua/micky/plugins/fugitive.lua
@@ -1,3 +1,6 @@
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>gs", ":Git<CR>")
+keymap.set("n", "<leader>gg", ":Git ", { noremap = true })
+keymap.set("n", "<leader>ggc", ":Git checkout ", { noremap = true })
+keymap.set("n", "<leader>ggb", ":Git bisect ", { noremap = true })
