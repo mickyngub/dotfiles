c0089caae756c16032eabeb5b43ae2e6ddd235e9
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-04-04 14:45:17 +0700

    :art: adds syntax highlighting for diffview

--- nvim/lua/micky/core/colorscheme.lua
+++ nvim/lua/micky/core/colorscheme.lua
@@ -5,3 +5,7 @@ if not status then
 	print("Colorscheme not found!")
 	return
 end
+vim.cmd([[highlight DiffAdd gui=none guifg=none guibg=#103235]])
+vim.cmd([[highlight DiffChange gui=none guifg=none guibg=#272D43]])
+vim.cmd([[highlight DiffText gui=none guifg=none guibg=#394b70]])
+vim.cmd([[highlight DiffDelete gui=none guifg=none guibg=#3F2D3D]])
