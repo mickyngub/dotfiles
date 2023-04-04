59cd95d3571d03b3323f9bff193e085f09b7bdab
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-04-04 13:18:19 +0700

    :sparkles: adds keymaps for diffview history and file

--- nvim/lua/micky/plugins/diffview.lua
+++ nvim/lua/micky/plugins/diffview.lua
@@ -3,3 +3,5 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>dv", ":DiffviewOpen ", { noremap = true })
 keymap.set("n", "<leader>dvo", ":DiffviewOpen<CR>", { noremap = true })
 keymap.set("n", "<leader>dvc", ":DiffviewClose<CR>", { noremap = true })
+keymap.set("n", "<leader>dvh", ":DiffviewFileHistory<CR>", { noremap = true })
+keymap.set("n", "<leader>dvf", ":DiffviewFileHistory %<CR>", { noremap = true })
