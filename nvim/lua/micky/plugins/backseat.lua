8f872129b49a28f3cfb30e8630ff52d34db6032e
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-04-05 13:50:08 +0700

    ➕ adds backseat

---
+++ nvim/lua/micky/plugins/backseat.lua
@@ -0,0 +1,6 @@
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>bsb", ":Backseat<CR>")
+keymap.set("n", "<leader>bsa", ":BackseatAsk ")
+keymap.set("n", "<leader>bscc", ":BackseatClear<CR>")
+keymap.set("n", "<leader>bscl", ":BackseatClearLine<CR>")
