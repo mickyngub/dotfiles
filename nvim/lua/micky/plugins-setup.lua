829ff114ade1e050562fbb16e6dffae398f3ca0a
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-12-03 19:21:05 +0700

    :heavy_minus_sign: removes backseat

--- nvim/lua/micky/plugins-setup.lua
+++ nvim/lua/micky/plugins-setup.lua
@@ -218,25 +218,6 @@ require("lazy").setup({
 	},
 	"ThePrimeagen/git-worktree.nvim",
 	{
-		"james1236/backseat.nvim",
-		config = function()
-			if not OPENAI_API_KEY then
-				print("Please provide an OpenAI API key for ChatGPT plugin.")
-				return
-			end
-			require("backseat").setup({
-				-- Alternatively, set the env var $OPENAI_API_KEY by putting "export OPENAI_API_KEY=sk-xxxxx" in your ~/.bashrc
-				openai_api_key = OPENAI_API_KEY, -- Get yours from platform.openai.com/account/api-keys
-				openai_model_id = "gpt-3.5-turbo", --gpt-4 (If you do not have access to a model, it says "The model does not exist")
-				-- split_threshold = 100,
-				-- additional_instruction = "Respond snarkily", -- (GPT-3 will probably deny this request, but GPT-4 complies)
-				highlight = {
-					group = "Todo",
-				},
-			})
-		end,
-	},
-	{
 		"jackMort/ChatGPT.nvim",
 		event = "VeryLazy",
 		config = function()
