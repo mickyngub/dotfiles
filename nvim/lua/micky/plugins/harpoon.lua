7836ee35a742a294ae8fa7be72b0a316c63161d9
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-12-04 16:58:57 +0700

    :wrench: configures harpoon keymap

--- nvim/lua/micky/plugins/harpoon.lua
+++ nvim/lua/micky/plugins/harpoon.lua
@@ -11,10 +11,4 @@ end
 local keymap = vim.keymap -- for conciseness
 
 keymap.set("n", "<leader>a", mark.add_file)
-keymap.set("n", "<leader>he", ui.toggle_quick_menu)
-keymap.set("n", "<leader>h1", function()
-	ui.nav_file(1)
-end)
-keymap.set("n", "<leader>h2", function()
-	ui.nav_file(2)
-end)
+keymap.set("n", "<leader>ha", ui.toggle_quick_menu)
