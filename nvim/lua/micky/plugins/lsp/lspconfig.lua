712f6d1d0cf9e37c89bb8e16d23e4651d2835a28
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-12-09 16:04:23 +0700

    :wrench: adds lsp keymaps

--- nvim/lua/micky/plugins/lsp/lspconfig.lua
+++ nvim/lua/micky/plugins/lsp/lspconfig.lua
@@ -44,6 +44,8 @@ local on_attach = function(client, bufnr)
 	keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
 	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
 	keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+	keymap.set("n", "<leader>R", "<cmd>LspRestart<CR>", opts)
+	keymap.set("n", "<leader>lsp", "<cmd>LspInfo<CR>", opts)
 
 	-- typescript specific keymaps (e.g. rename file and update imports)
 	if client.name == "tsserver" then
