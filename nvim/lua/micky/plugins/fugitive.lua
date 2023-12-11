7933652efc0287f4d3d591847cc61a48f53b8ec1
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-12-11 18:41:48 +0700

    :wrench: updates fugitive keymaps

--- nvim/lua/micky/plugins/fugitive.lua
+++ nvim/lua/micky/plugins/fugitive.lua
@@ -1,8 +1,8 @@
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>gs", ":Git<CR>")
-keymap.set("n", "<leader>gg", ":Git ", { noremap = true })
-keymap.set("n", "<leader>ggc", ":Git checkout ", { noremap = true })
-keymap.set("n", "<leader>ggb", ":Git bisect ", { noremap = true })
-keymap.set("n", "<leader>ggp", ":Git pull ", { noremap = true })
-keymap.set("n", "<leader>ggP", ":Git push ", { noremap = true })
+keymap.set("n", "<leader>g", ":Git ", { noremap = true })
+keymap.set("n", "<leader>gc", ":Git checkout ", { noremap = true })
+keymap.set("n", "<leader>gb", ":Git bisect ", { noremap = true })
+keymap.set("n", "<leader>gp", ":Git pull ", { noremap = true })
+keymap.set("n", "<leader>gP", ":Git push ", { noremap = true })
