8f1a08f694b24b8a25bf491bcae04edf88599bdc
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-12-12 22:54:33 +0700

    :wrench: fixes conflicting keymap <leader>gb

--- nvim/lua/micky/plugins/fugitive.lua
+++ nvim/lua/micky/plugins/fugitive.lua
@@ -3,6 +3,6 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>gs", ":Git<CR>")
 keymap.set("n", "<leader>g", ":Git ", { noremap = true })
 keymap.set("n", "<leader>gc", ":Git checkout ", { noremap = true })
-keymap.set("n", "<leader>gb", ":Git bisect ", { noremap = true })
+keymap.set("n", "<leader>gB", ":Git bisect ", { noremap = true })
 keymap.set("n", "<leader>gp", ":Git pull ", { noremap = true })
 keymap.set("n", "<leader>gP", ":Git push ", { noremap = true })
