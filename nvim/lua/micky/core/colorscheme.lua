e9de92053e19c64a9f8385aac7b380c3df928acf
Pichaya Puttekulangkura <micky47561@gmail.com>
2023-12-24 18:27:32 +0700

    :art: adds kanagawa, nordic, and tokyonight colorscheme

--- nvim/lua/micky/core/colorscheme.lua
+++ nvim/lua/micky/core/colorscheme.lua
@@ -1,10 +1,10 @@
-local status, _ = pcall(vim.cmd, "colorscheme onedark")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+local status, _ = pcall(vim.cmd, "colorscheme kanagawa")
 if not status then
 	print("Colorscheme not found!")
 	return
 end
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.cmd([[highlight DiffAdd gui=none guifg=none guibg=#103235]])
 vim.cmd([[highlight DiffChange gui=none guifg=none guibg=#272D43]])
 vim.cmd([[highlight DiffText gui=none guifg=none guibg=#394b70]])
