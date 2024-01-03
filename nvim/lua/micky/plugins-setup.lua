6e0b6292548ebf3cbc5255989e60b4e8cd2fa62a
Pichaya Puttekulangkura <micky47561@gmail.com>
2024-01-03 13:57:08 +0700

    :wrench: uses harpoon2

--- nvim/lua/micky/plugins-setup.lua
+++ nvim/lua/micky/plugins-setup.lua
@@ -138,12 +138,12 @@ require("lazy").setup({
 	-- harpoon
 	{
 		"theprimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
 		config = function()
-			require("harpoon").setup({
-				menu = {
-					width = vim.api.nvim_win_get_width(0) - 40,
-				},
-			})
+			require("harpoon").setup()
 		end,
 	},
 	-- undotree
--- nvim/lua/micky/plugins/harpoon.lua
+++ nvim/lua/micky/plugins/harpoon.lua
@@ -1,14 +1,9 @@
-local harpoon_mark_status, mark = pcall(require, "harpoon.mark")
-if not harpoon_mark_status then
-	return
-end
-
-local harpoon_ui_status, ui = pcall(require, "harpoon.ui")
-if not harpoon_ui_status then
-	return
-end
-
+local harpoon = require("harpoon")
 local keymap = vim.keymap -- for conciseness
 
-keymap.set("n", "<leader>a", mark.add_file)
-keymap.set("n", "<leader>ha", ui.toggle_quick_menu)
+keymap.set("n", "<leader>a", function()
+	harpoon:list():append()
+end)
+keymap.set("n", "<leader>ha", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
