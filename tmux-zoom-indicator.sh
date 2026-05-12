fc77a1a5492cfc2e9c5e08f1f02431cd672b8a01
Pichaya Puttekulangkura <micky47561@gmail.com>
2026-05-12 23:08:54 +0700

    :sparkles: add tmux zoom focus indicator

---
+++ tmux-zoom-indicator.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Toggle iTerm2 background tint based on tmux zoom state.
+# Sent through tmux DCS passthrough so the escape reaches the terminal emulator.
+
+tty=$(tmux display-message -p '#{pane_tty}')
+zoomed=$(tmux display-message -p '#{window_zoomed_flag}')
+
+if [ "$zoomed" = "1" ]; then
+  printf '\033Ptmux;\033\033]11;rgb:2a/2a/00\007\033\\' > "$tty"
+else
+  printf '\033Ptmux;\033\033]111\007\033\\' > "$tty"
+fi
