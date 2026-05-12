40e75b7e6e765f9942e5ad3965523ee96c4eb803
Pichaya Puttekulangkura <micky47561@gmail.com>
2026-05-13 00:22:51 +0700

    :truck: move nvim config into .config/

--- init.sh
+++ init.sh
@@ -83,7 +83,7 @@ echo "==> Creating symlinks..."
 mkdir -p ~/.config
 
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zsh_env_vars ~/.zsh_env_vars
 ln -sf ~/dotfiles/.config/herdr ~/.config/herdr
